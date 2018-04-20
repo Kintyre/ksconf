7c3adec6e2f6bfe6137364777b8ec01a944b8e62
Lowell Alleman <lowell@kintyre.co>
2018-04-20 08:34:42 -0400

    Release v1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name="KintyreSplunkConfTool",
-      version="1.0",
+      version="1.0.1",
       description="Kintyre's Splunk Configuration Tool",
       author="Lowell Alleman",
       author_email="lowell@kintyre.co",
