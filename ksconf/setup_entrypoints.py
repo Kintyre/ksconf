aeea26aff32810482aea73bba32cad6511adf822
Lowell Alleman <lowell@kintyre.co>
2019-02-08 19:55:24 -0500

    Merge csv table creation into make_dyn_docs script
    Consolidate csv creation script into the make_dyn_docs script (that used to
    make just the CLI docs).  Did some refactoring and some unicode fixes (Python 2
    vs 3) and lack of support for unicode for the PY2 version, blah blah blah...

--- ksconf/setup_entrypoints.py
+++ ksconf/setup_entrypoints.py
@@ -44,6 +44,32 @@ def get_entrypoints_setup():
         setup[group] = [ "{0.name} = {0.module_name}:{0.object_name}".format(ep) for ep in entries ]
     return setup
 
+import csv
+from ksconf.commands import get_entrypoints
+
+
+def unwrap(s):
+    return s.replace("\n", " ")
+
+def build_csv(out_file):
+
+    with open(out_file, "w") as stream:
+        csvwriter = csv.writer(stream, dialect=csv.QUOTE_ALL)
+        # XXX: Should refactor?   Borrowed from ksconf/__main__.py
+
+        for (name, entry) in get_entrypoints("ksconf_cmd").items():
+            # Pros/conf links to the doc vs 'ref'?
+            #ref_template = ":doc:`cmd_{}`"
+            ref_template = ":ref:`ksconf {0} <ksconf_cmd_{0}>`"
+            cmd_cls = entry.load()
+            row = [ ref_template.format(name), cmd_cls.maturity, unwrap(cmd_cls.help) ]
+            csvwriter.writerow( row)
+
+
+
+if __name__ == '__main__':
+    build_csv("docs/source/dyn/ksconf_subcommands.csv")
+
 
 class LocalEntryPoint(object):
     """ Bare minimum standin for entrypoints.EntryPoint """
--- make_dyn_docs.py
+++ make_dyn_docs.py
@@ -7,6 +7,9 @@ import os
 import re
 import sys
 from subprocess import Popen, PIPE
+from functools import wraps
+
+from six import PY2
 
 # Prevent installed (likely older) version of ksconf from taking over
 project_dir = os.path.dirname(os.path.abspath(__file__ or sys.argv[0]))
@@ -25,6 +28,7 @@ def cmd_output(*cmd):
     return stdout.decode("utf-8").splitlines()
 
 
+
 def parse_subcommand(lines):
     text = "\n".join(lines)
     match = re.search(r'[\r\n]+positional arguments:\s*\{([\w,-]+)\}', text)
@@ -33,14 +37,16 @@ def parse_subcommand(lines):
         return subcommands
     return []
 
+
 def prefix(iterable, indent=4):
     p = " " * indent
     for line in iterable:
         yield p + line
 
+
 def restructured_header(header, level):
-    level_symbols= '#*=-^"~'
-    char = level_symbols[level-1]
+    level_symbols = '#*=-^"~'
+    char = level_symbols[level - 1]
     return "{}\n{}\n".format(header, char * len(header))
 
 
@@ -65,22 +71,68 @@ def write_doc_for(stream, cmd, level=2, cmd_name=None, level_inc=1, *subcmds):
         write_doc_for(stream, cmd, level + level_inc, cmd_name, level_inc, *sc)
 
 
-readme_file = os.path.join(project_dir, "docs", "source", "dyn", "cli.rst")
-readme = ReluctantWriter(readme_file, "w", newline="\n", encoding="utf-8")
+def show_changes(f):
+    @wraps(f)
+    def wrapper(path):
+        rw = f(path)
+        if rw.result == "created":
+            print("Make fresh {}".format(path))
+        elif rw.result == "unchanged":
+            print("No changes made to {}.".format(path))
+        elif rw.result == "updated":
+            print("{} updated".format(path))
+        if rw.change_needed:
+            return 1
+        return 0
+    return wrapper
 
-with readme as stream:
-    stream.write(restructured_header("Command line reference", 1))
-    stream.write("\n\nKSCONF supports the following CLI options:\n\n")
-    print("Building docs for ksconf")
-    write_doc_for(stream, ["-m", "ksconf"], cmd_name="ksconf", level=2, level_inc=0)
 
-if readme.result == "created":
-    print("Make fresh {}".format(readme_file))
-elif readme.result == "unchanged":
-    print("No changes made to file.")
-elif readme.result == "updated":
-    print("{} updated".format(readme_file))
+@show_changes
+def make_cli_docs(readme_file):
+    readme = ReluctantWriter(readme_file, "w", newline="\n", encoding="utf-8")
+    with readme as stream:
+        stream.write(restructured_header("Command line reference", 1))
+        stream.write("\n\nKSCONF supports the following CLI options:\n\n")
+        print("Building docs for ksconf")
+        write_doc_for(stream, ["-m", "ksconf"], cmd_name="ksconf", level=2, level_inc=0)
+    return readme
 
-# Return a non-0 exit code to tell pre-commit that changes were made (abort current commit session)
-if readme.change_needed:
-    sys.exit(1)
+
+
+@show_changes
+def make_subcommands_table(csv_path):
+    import csv
+    from ksconf.commands import get_entrypoints
+    if PY2:
+        table = ReluctantWriter(csv_path, "wb")
+    else:
+        table = ReluctantWriter(csv_path, "w", encoding="utf-8")
+    with table as stream:
+        csvwriter = csv.writer(stream, dialect=csv.QUOTE_NONNUMERIC)
+        for (name, entry) in get_entrypoints("ksconf_cmd").items():
+            # Pros/conf links to the doc vs 'ref'?
+            #ref_template = ":doc:`cmd_{}`"
+            ref_template = ":ref:`ksconf {0} <ksconf_cmd_{0}>`"
+            cmd_cls = entry.load()
+            row = [
+                ref_template.format(name),
+                cmd_cls.maturity,
+                cmd_cls.help.replace("\n", " "),
+            ]
+            # Workaround csv module not supporting unicode in PY2
+            if PY2:
+                row = [ s.encode("utf-8") for s in row ]
+            csvwriter.writerow(row)
+    return table
+
+
+if __name__ == '__main__':
+    docs_dir = lambda filename: os.path.join(project_dir, "docs", "source", "dyn", filename)
+    changes = 0
+
+    changes += make_cli_docs(docs_dir("cli.rst"))
+    changes += make_subcommands_table(docs_dir("ksconf_subcommands.csv"))
+
+    # Return a non-0 exit code to tell pre-commit that changes were made (abort current commit session)
+    if changes:
+        sys.exit(1)
