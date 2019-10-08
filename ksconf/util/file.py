088219c142f4ec58ac55308d255f3682e3984c90
Lowell Alleman <lowell@kintyre.co>
2019-10-08 17:18:59 -0400

    Fix bug with 'git' detection on Windows
    - Determined that the simple "which()" fallback function I was using wasn't
      fancy enough for Windows due to file extension handling.  So I stole the
      entire shutil.which() function from the Python 3.7 standard library!  (Can't
      wait to make all this Python-3 only!)

--- ksconf/util/file.py
+++ ksconf/util/file.py
@@ -18,11 +18,62 @@ try:
     # Builtin in Python 3.3+
     from shutil import where
 except ImportError:
-    def which(file):
-        for path in os.environ.get("PATH", os.defpath).split(os.pathsep):
-            if os.path.exists(os.path.join(path, file)):
-                return os.path.join(path, file)
+    # Borrowed from Python's stdlib 3.7 shutil.py; comments removed to keep this short
+
+    def which(cmd, mode=os.F_OK | os.X_OK, path=None):
+        """Given a command, mode, and a PATH string, return the path which
+        conforms to the given mode on the PATH, or None if there is no such
+        file.
+        `mode` defaults to os.F_OK | os.X_OK. `path` defaults to the result
+        of os.environ.get("PATH"), or can be overridden with a custom search
+        path.
+        """
+        def _access_check(fn, mode):
+            return (os.path.exists(fn) and os.access(fn, mode)
+                    and not os.path.isdir(fn))
+        if os.path.dirname(cmd):
+            if _access_check(cmd, mode):
+                return cmd
+            return None
+
+        if path is None:
+            path = os.environ.get("PATH", None)
+            if path is None:
+                try:
+                    path = os.confstr("CS_PATH")
+                except (AttributeError, ValueError):
+                    # os.confstr() or CS_PATH is not available
+                    path = os.defpath
+        # PATH='' doesn't match, whereas PATH=':' looks in the current directory
+        if not path:
+            return None
+        path = path.split(os.pathsep)
+
+        if sys.platform == "win32":
+            # The current directory takes precedence on Windows.
+            if not os.curdir in path:
+                path.insert(0, os.curdir)
+
+            # PATHEXT is necessary to check on Windows.
+            pathext = os.environ.get("PATHEXT", "").split(os.pathsep)
+            if any(cmd.lower().endswith(ext.lower()) for ext in pathext):
+                files = [cmd]
+            else:
+                files = [cmd + ext for ext in pathext]
+        else:
+            files = [cmd]
+
+        seen = set()
+        for dir in path:
+            normdir = os.path.normcase(dir)
+            if not normdir in seen:
+                seen.add(normdir)
+                for thefile in files:
+                    name = os.path.join(dir, thefile)
+                    if _access_check(name, mode):
+                        return name
         return None
+
 
 
 def _is_binary_file(filename, peek=2048):
